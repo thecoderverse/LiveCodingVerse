81938babe0e0478cea8858e0d4ae4e0b1c2f16ec
Sümeyye Akkaş <akkasumeyye@hotmail.com>
2023-05-22 23:51:37 +0300

    degisiklikler

--- rating-system/pages/login/index.tsx
+++ rating-system/pages/login/index.tsx
@@ -1,39 +1,57 @@
-import { User } from "@prisma/client"
-import { useRouter } from "next/router"
-import { useState } from "react"
-import { fetchPost } from "../../fetch-helpers/post"
+import { User } from "@prisma/client";
+import { useRouter } from "next/router";
+import React from 'react';
+import { fetchPost } from "../../fetch-helpers/post";
 
-const Login = () => {
-    const router = useRouter()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value)
-    }
-
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-    }
-
-    const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        const result = await fetchPost<User>('/api/login', { email, password } as User)
-        if (result && result.id) {
-            router.push('/home')
-        }
-    }
-    
-    return (
-        <>
-            <h1>Login</h1>
-            <form>
-                <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
-                <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
-                <button type="submit" onClick={handleSubmit}>Submit</button>
-            </form>
-        </>
-    )
+interface UserData {
+  email: string;
+  password: string;
 }
 
-export default Login
+const Login = () => {
+  const router = useRouter();
+  const [userData, setUserData] = React.useState<UserData>({email: "", password: ""});
+
+  const handleChange = <T extends keyof UserData>(
+    field: T,
+    value: UserData[T]
+  ): void => {
+    setUserData({ ...userData, [field]: value });
+  };
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLButtonElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    console.log(userData);
+    try {
+      const result = await fetchPost<User>("/api/login", userData);
+      if (result && result.id) {
+        router.push("/home");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="email"
+          value={userData.email}
+          onChange={(e) => handleChange("email", e.target.value)}
+        />
+        <input
+          type="password"
+          value={userData.password}
+          onChange={(e) => handleChange("password", e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- rating-system/pages/signup/index.tsx
+++ rating-system/pages/signup/index.tsx
@@ -1,43 +1,58 @@
-import { User } from '@prisma/client'
-import { useRouter } from 'next/router'
-import React from 'react'
-import { fetchPost } from '../../fetch-helpers/post'
+import { User } from '@prisma/client';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { fetchPost } from '../../fetch-helpers/post';
 
-const Signup = () => {
-  const router = useRouter()
-  const [name, setName] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
+interface UserData {
+  name: string;
+  email: string;
+  password: string;
+}
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value)
-  }
+const Signup: React.FC = () => {
+  const router = useRouter();
+  const [userData, setUserData] = React.useState<UserData>({ name: '', email: '', password: '' });
+  const [isEmailValid, setIsEmailValid] = React.useState<boolean>(true);
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-  }
+  const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
 
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
-
-  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    const result = await fetchPost<User>('/api/user', { name, email, password } as User)
-    if (result && result.id) {
-      router.push('/login')
+  const handleChange = <T extends keyof UserData>(field: T, value: UserData[T]): void => {
+    if (field === 'email') {
+      const isValid = validateEmail(value);
+      setIsEmailValid(isValid);
     }
-  }
+    setUserData((prevUserData) => ({ ...prevUserData, [field]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>): Promise<void> => {
+    e.preventDefault();
+    if (!isEmailValid) {
+      alert('Lütfen geçerli bir e-posta adresi girin.');
+      return;
+    }
+    console.log(userData);
+    try {
+      const result = await fetchPost<User>('/api/user', userData);
+      if (result && result.id) {
+        router.push('/login');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <div>
       <h1>Rating System</h1>
-      <input type="text" value={name} onChange={handleNameChange} />
-      <input type="email" value={email} onChange={handleEmailChange} />
-      <input type="password" value={password} onChange={handlePasswordChange} />
+      <input type="text" value={userData.name} onChange={(e) => handleChange('name', e.target.value)} />
+      <input type="email" value={userData.email} onChange={(e) => handleChange('email', e.target.value)} />
+      <input type="password" value={userData.password} onChange={(e) => handleChange('password', e.target.value)} />
       <button onClick={handleSubmit}>Submit</button>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
